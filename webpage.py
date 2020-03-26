c322862ae6a1406ae3143748a2ab50a082aef524
cormacKrum <jgoel92@gmail.com>
2020-03-26 18:30:10 +0530

    My First Commit

---
+++ webpage.py
@@ -0,0 +1,16 @@
+from flask import Flask, templating
+
+app = Flask(__name__)
+
+
+@app.route('/privacypolicy/')
+def privacypolicy():
+    return templating.render_template('privacy.html')
+
+
+@app.route('/termsandconditions/')
+def termsandc():
+    return templating.render_template('tnc.html')
+
+
+app.run()
